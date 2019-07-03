e4c39665e75a9bc0036fd4c9230f181e9d407188
Daniele Manca <danimanca2010@gmail.com>
2019-07-03 19:19:09 +0800

    Added styles for the playlist play video screen, added animation to video blocks on homepage

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Video = ({video, onClose}) => {
   return(
-    <main className="yt-playlist">
-      <div>
-      <iframe width="560" height="315" src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      
+    <main className="yt-playlist yt-playlist--flex">
+      <div className="yt-playlist__video__play">
+        <iframe width="100%" height="400" src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
-      <a href="" onClick={onClose}>Back</a>
+      <div className="yt-playlist__video__info">
+        <h2 className="heading-primary">{video.snippet.title}</h2>
+        <p className="yt-playlist__video__info__desc">{video.snippet.description}</p>
+        <p className="yt-playlist__video__info__published-date">Published on {Date.parse(video.snippet.publishedAt)}</p>
+        <a className="yt-playlist__video__info__back" href="#" onClick={onClose}>&#9664; Back to list</a>
+      </div>
     </main>
   )
 }

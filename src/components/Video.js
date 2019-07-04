5ef45ab24d0518e875abd20a786d38de3739f827
Daniele Manca <danimanca2010@gmail.com>
2019-07-04 16:23:23 +0800

    Added flexbox styling for layout to support up to IE11, added media queries for responsive behavior

--- src/components/Video.js
+++ src/components/Video.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Video = ({video, onClose}) => {
   return(
-    <main className="yt-playlist yt-playlist--flex">
+    <main className="yt-playlist">
       <div className="yt-playlist__video__play">
         <iframe width="100%" height="400" src={`https://www.youtube.com/embed/${video.snippet.resourceId.videoId}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </div>
